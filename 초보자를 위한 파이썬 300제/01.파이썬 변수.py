5b372da09b570551b388c2a36a1a48569b630427
dongseup <dongseup83@gmail.com>
2022-08-17 00:30:23 +0900

    파이썬 시작하기 update

--- 초보자를 위한 파이썬 300제/01.파이썬 변수.py
+++ 초보자를 위한 파이썬 300제/01.파이썬 변수.py
@@ -1,2 +1,29 @@
-# print 기초
+# 001 print 기초
 print('Hello World')
+
+# 002 print 기초
+print("Mary's cosmetics")
+
+# 003 print 기초
+print('신씨가 소리질렀다. "도둑이야".')
+
+# 004 print 기초
+print('"C:\Windows"')
+
+# 005 print 탭과 줄바꿈
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+
+# 006 print 여러 데이터 출력
+print("오늘은", "일요일")
+
+# 007 print 기초
+print('naver', 'kakao', 'sk', 'samsung', sep=';')
+
+# 008 print 기초
+print('naver', 'kakao', 'sk', 'samsung', sep='/')
+
+# 009 print 줄바꿈
+print("first", end=""); print("second")
+
+# 010 연산 결과 출력
+print(5/3)
