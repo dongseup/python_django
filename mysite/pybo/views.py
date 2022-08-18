457cf98a812aeeebfdc5437766ea00ff15eb676f
dongseup <dongseup83@gmail.com>
2022-08-18 15:55:28 +0900

    2-04. 조회와 템플릿

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,7 +1,15 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 from pybo.models import Question
 
 
 def index(request):
     question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
