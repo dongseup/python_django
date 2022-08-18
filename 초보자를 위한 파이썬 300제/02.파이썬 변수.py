f7c5831c038ffb577f5cdcda1052b3dc3a5d1a13
dongseup <dongseup83@gmail.com>
2022-08-18 09:49:55 +0900

    연습문제 업데이트

---
+++ 초보자를 위한 파이썬 300제/02.파이썬 변수.py
@@ -0,0 +1,49 @@
+# 011 변수 사용하기
+삼성전자 = 50000
+총평가금액 = 삼성전자 * 10
+print(총평가금액)
+
+# 012 변수 사용하기
+시가총액 = 2980000000000
+현재가 = 50000
+PER = 15.79
+print(시가총액, type(시가총액))
+print(현재가, type(현재가))
+print(PER, type(PER))
+
+# 013 문자열 출력
+s = 'hello'
+t = 'python'
+print(s +'!', t)
+
+# 014 파이썬을 이용한 값 계산
+print(2+2*3)
+
+# 015 type 함수
+a = '132'
+print(type(a))
+
+# 016 문자열을 정수로 변환
+num_str = '720'
+print(int(num_str), type(int(num_str)))
+
+# 017 정수를 문자열 100으로 변환
+num = 100
+print(str(num), type(str(num)))
+
+# 018 문자열을 실수로 변환
+num_str = '15.79'
+print(float(num_str), type(float(num_str)))
+
+# 019 문자열을 정수로 변환
+year = '2020'
+print(int(year), type(int(year)))
+print(int(year) + 1)
+print(int(year) + 2)
+print(int(year) + 3)
+
+# 020 파이썬 계산
+month_price = 48584
+total_month = 36
+total_price = month_price * total_month
+print(total_price)
