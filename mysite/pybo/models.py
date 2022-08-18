1ab98d070467d140e160cd3e8daf6d3cbac2d469
dongseup <dongseup83@gmail.com>
2022-08-18 09:48:06 +0900

    연제문제풀이

--- mysite/pybo/models.py
+++ mysite/pybo/models.py
@@ -15,3 +15,6 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.content
--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 
+from pybo.models import Question
+
 
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    question_list = Question.objects.order_by('-create_date')
