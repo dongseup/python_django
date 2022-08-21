b3dda7ed2376a2be0e9a752a5294e147735a6552
dongseup <dongseup83@gmail.com>
2022-08-21 19:45:21 +0900

    연습문제 업데이트

--- 초보자를 위한 파이썬 300제/03.파이썬 문자열.py
+++ 초보자를 위한 파이썬 300제/03.파이썬 문자열.py
@@ -43,3 +43,48 @@ string = 'abcd'
 b = string.replace('b', 'B')
 print(string)
 print(b)
+
+# 031 문자열 합치기
+a = '3'
+b= '4'
+print(a + b)
+
+# 032 문자열 곱하기
+print('Hi' * 3)
+
+# 033 문자열 곱하기
+print('-' * 80)
+
+# 034 문자열 곱하기
+t1 = 'python'
+t2 = 'java'
+print((t1 + ' ' + t2 + ' ') * 4)
+
+# 035 문자열 출력
+name1 = '김민수'
+age1 = 10
+name2 = '이철희'
+age2 = 13
+print('이름: %s 나이: %d' % (name1, age1))
+print('이름: %s 나이: %d' % (name2, age2))
+
+# 036 문자열 출력
+print('이름: {0} 나이: {1}'.format(name1, age1))
+print('이름: {0} 나이: {1}'.format(name2, age2))
+
+# 037 문자열 출력
+print(f'이름: {name1} 나이: {age1}')
+print(f'이름: {name2} 나이: {age2}')
+
+# 038 컴마 제거하기
+상장주식수 = "5,969,782,550"
+re =상장주식수.replace(',', '')
+print(int(re), type(re))
+
+# 039 문자열 슬라이싱
+분기 = "2020/03(E) (IFRS연결)"
+print(분기[:7])
+
+#040 strip 메서드
+data = "   삼성전자    "
+print(data.strip())
