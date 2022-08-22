d6c603abda6ced2d98f23bdce3df2cb816f833a7
lim dongseup <dongseup_lim@gonoone.com>
2022-08-22 19:09:38 +0900

    연습문제 업데이트

--- 초보자를 위한 파이썬 300제/03.파이썬 문자열.py
+++ 초보자를 위한 파이썬 300제/03.파이썬 문자열.py
@@ -88,3 +88,43 @@ print(분기[:7])
 #040 strip 메서드
 data = "   삼성전자    "
 print(data.strip())
+
+# 041 upper 메서드
+ticker = "btc_krw"
+print(ticker.upper())
+
+# 042 lower 메서드
+ticker = "BTC_KRW"
+print(ticker.lower())
+
+# 043 capitalize 메서드
+hello = 'hello'
+print(hello.capitalize())
+
+# 044 endswith 메서드
+file_name = "보고서.xlsx"
+print(file_name.endswith('xlsx'))
+
+# 045 endswith 메서드
+file_name = "보고서.xlsx"
+print(file_name.endswith(('xlsx', 'xls')))
+
+# 046 startswith 메서드
+file_name = "2020_보고서.xlsx"
+print(file_name.startswith('2020'))
+
+# 047 split 메서드
+a = "hello world"
+print(a.split(' '))
+
+# 048 split 메서드
+ticker = "btc_krw"
+print(ticker.split('_'))
+
+# 049 split 메서드
+date = "2020-05-01"
+print(date.split("-"))
+
+# 050 rstrip 메서드
+data = "039490     "
+print(data.strip())
---
+++ 초보자를 위한 파이썬 300제/04.파이썬 리스트.py
@@ -0,0 +1,44 @@
+# 051 리스트 생성
+movie_rank = ['닥터 스트레인지', '스플릿', '럭키']
+print(movie_rank)
+
+# 052 리스트에 원소 추가
+movie_rank.append("배트맨")
+print(movie_rank)
+
+# 053
+movie_rank = ['닥터 스트레인지', '스플릿', '럭키', '배트맨']
+movie_rank.insert(0, '슈퍼맨')
+print(movie_rank)
+
+# 054
+movie_rank = ['닥터 스트레인지', '슈퍼맨', '스플릿', '럭키', '배트맨']
+del movie_rank[3]
+print(movie_rank)
+
+# 055
+movie_rank = ['닥터 스트레인지', '슈퍼맨', '스플릿', '배트맨']
+del movie_rank[2:]
+print(movie_rank)
+
+# 056
+lang1 = ["C", "C++", "JAVA"]
+lang2 = ["Python", "Go", "C#"]
+print(lang1 + lang2)
+
+# 057
+nums = [1, 2, 3, 4, 5, 6, 7]
+print(max(nums))
+print(min(nums))
+
+# 058
+nums = [1, 2, 3, 4, 5]
+print(sum(nums))
+
+# 059
+cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "소시지", "라면", "팥빙수", "김치전"]
+print(len(cook))
+
+# 060
+nums = [1, 2, 3, 4, 5]
+print(sum(nums) / len(nums))
