59ab8f560ba83b2cbc23f8b3af8eda849ce700d9
lim dongseup <dongseup_lim@gonoone.com>
2022-08-23 20:54:38 +0900

    가상환경 새롭게 만들고 장고 연습 파일 업데이트

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -10,6 +10,7 @@ def index(request):
 
 
 def detail(request, question_id):
+    # question = Question.objects.get(id=question_id)
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
