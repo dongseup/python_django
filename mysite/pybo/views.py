70268c7fdbfafa18b8d4424c855211ddb263fe3e
dongseup <dongseup83@gmail.com>
2022-08-25 16:52:37 +0900

    점프투장고 2-10 폼 스터디

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponseNotAllowed
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from pybo.models import Question, Answer
+from pybo.forms import QuestionForm, AnswerForm
 
 
 def index(request):
@@ -18,7 +20,31 @@ def detail(request, question_id):
 
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    # question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
-    answer.save()
-    return redirect('pybo:detail', question_id=question.id)
+
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        return HttpResponseNotAllowed('Only POST is possible.')
+
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
+
+
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+
+    return render(request, 'pybo/question_form.html', {'form': form})
