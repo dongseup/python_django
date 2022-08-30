d078b6a051a596bdfa59b5a24a56be5397f53012
dongseup <dongseup83@gmail.com>
2022-08-30 21:33:40 +0900

    07. 파이썬 분기문 ~115

---
+++ 초보자를 위한 파이썬 300제/07. 파이썬 분기문.py
@@ -0,0 +1,110 @@
+#101
+# bool타입
+
+#102
+print(3 == 5) # false
+
+#103
+print(3 < 5) # true
+
+#104
+x = 4
+print(1 < x < 5)
+
+#105
+print ((3 == 3) and (4 != 3))
+
+#106
+print(3 >= 4)
+
+
+#107
+if 4 < 3:
+    print("Hello World")
+
+#108
+if 4 < 3:
+    print("Hello World.")
+else:
+    print("Hi, there.")
+
+
+#109
+if True :
+    print ("1")
+    print ("2")
+else :
+    print("3")
+print("4")
+
+
+#110
+if True :
+    if False:
+        print("1")
+        print("2")
+    else:
+        print("3")
+else :
+    print("4")
+print("5")
+
+#111
+# user = input('입력:')
+# print(user*2)
+
+#112
+# num = input('숫자를 입력하세여:')
+# print(int(num) + 10)
+
+#113
+# num = input('숫자:')
+# print("짝수" if int(num)%2 == 0 else "홀수")
+
+#114
+# user = input('입력값: ')
+# num = 20 + int(user)
+# if num > 255:
+#     print(255)
+# else:
+#     print(num)
+
+#115
+# user = input('입력값: ')
+# num = int(user) - 20
+# if num < 0:
+#     print(0)
+# elif num > 255:
+#     print(255)
+# else:
+#     print(num)
+
+#116
+
+#117
+
+#118
+
+#119
+
+#120
+
+#121
+
+#122
+
+#123
+
+#124
+
+#125
+
+#126
+
+#127
+
+#128
+
+#129
+
+#130
