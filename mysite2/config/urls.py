ddb71d81c0d87cff68530d04183d9e1f4b34460b
dongseup <dongseup83@gmail.com>
2022-08-31 14:48:04 +0900

    3-05 로그인과 로그아웃

--- mysite2/config/urls.py
+++ mysite2/config/urls.py
@@ -20,5 +20,6 @@ from pybo import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
-
+    path('common/', include('common.urls')),
+    path('', views.index, name='index'),
 ]
