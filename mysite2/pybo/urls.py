ff4caf76c4fb9e1beab244775e940a26d08bbb60
dongseup <dongseup83@gmail.com>
2022-09-06 14:49:01 +0900

    3-11 추천

--- mysite2/pybo/urls.py
+++ mysite2/pybo/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
     path('question/delete/<int:question_id>/', question_views.question_delete, name='question_delete'),
     path('answer/modify/<int:answer_id>/', answer_views.answer_modify, name='answer_modify'),
     path('answer/delete/<int:answer_id>/', answer_views.answer_delete, name='answer_delete'),
+    path('question/vote/<int:question_id>/', question_views.question_vote, name='question_vote'),
+    path('answer/vote/<int:answer_id>/', answer_views.answer_vote, name='answer_vote'),
+
 ]
