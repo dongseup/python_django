479282de9abaa08260ae16cc41f95d110b006125
dongseup_lim <dongseup83@gmail.com>
2022-09-13 00:03:05 +0900

    clone instagram #1

--- clone_instagram/clone_instagram/urls.py
+++ clone_instagram/clone_instagram/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from clone_instagram.views import Main
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Main.as_view())
 ]
