7c8890cd7493cd9522b34cf01ed52c27b5f7f7a9
dongseup_lim <dongseup83@gmail.com>
2022-09-15 19:03:58 +0900

    django restframework mixin 으로 구현

--- rest_framework_test/project/views.py
+++ rest_framework_test/project/views.py
@@ -1,55 +1,31 @@
-from django.http import Http404
-from django.shortcuts import render
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import mixins, generics
 
 from project.models import Blog
 from project.serializer import BlogSerializer
 
 
-# Create your views here.
-class BlogList(APIView):
-    # 블로그 리스트
-    def get(self, request):
-        blogs = Blog.objects.all()
-        serializer = BlogSerializer(blogs, many=True)
-        return Response(serializer.data)
+class BlogList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
 
-    # 새로운 블로그 글 작성
-    def post(self, request):
-        serializer = BlogSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class BlogDetail(APIView):
-    # 블로그 객체 가져오기
-    def get_object(self, pk):
-        try:
-            return Blog.objects.get(pk=pk)
-        except Blog.DoesNotExist:
-            raise Http404
+class BlogDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
 
-    # 블로그 디테일
-    def get(self, request, pk, format=None):
-        blog = self.get_object(pk)
-        serializer = BlogSerializer(blog)
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    # 블로스 디테일 수정
-    def put(self, request, pk, format=None):
-        blog = self.get_object(pk)
-        serializer = BlogSerializer(blog, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REEQUEST)
+    # Blog 수정하기
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    # 블로그 삭제
-    def delete(self, request, pk, format=None):
-        blog = self.get_object(pk)
-        blog.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    # Blog 삭제하기
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
