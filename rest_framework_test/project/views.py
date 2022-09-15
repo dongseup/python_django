d56acea1125b1a2a9e91e95df3e4253b09a0495e
dongseup_lim <dongseup83@gmail.com>
2022-09-15 19:09:13 +0900

    django restframework generic 으로 구현

--- rest_framework_test/project/views.py
+++ rest_framework_test/project/views.py
@@ -1,31 +1,15 @@
-from rest_framework import mixins, generics
+from .models import Blog
 
-from project.models import Blog
-from project.serializer import BlogSerializer
+from rest_framework import generics
+
+from .serializer import BlogSerializer
 
 
-class BlogList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class BlogList(generics.ListCreateAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class BlogDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class BlogDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    # Blog 수정하기
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    # Blog 삭제하기
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
