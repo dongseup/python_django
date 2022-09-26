68da6f33a488c5f8556233a1ed098fd9df180020
dongseup_lim <dongseup83@gmail.com>
2022-09-26 17:05:51 +0900

    todo rest framework todo 연습

---
+++ todo_restframework/apis/views.py
@@ -0,0 +1,13 @@
+from rest_framework import generics
+
+from todos import models
+from .serializers import TodoSerializer
+
+class ListTodo(generics.ListCreateAPIView):
+    queryset = models.Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Todo.objects.all()
+    serializer_class = TodoSerializer
--- todo_restframework/config/settings.py
+++ todo_restframework/config/settings.py
@@ -37,8 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todos'
+    'rest_framework',
+    'apis',
+    'todos',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
